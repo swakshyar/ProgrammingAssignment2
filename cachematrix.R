9289f64117e0765e603c4b1e8c5ba62a9ad58149
swakshyar <swakshyar.mishra@outlook.com>
2016-10-20 14:52:31 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Code below illustrates the lexical scoping concept using 2 functions that first creates a special object 
+##that stores a matrix and the second function caches the inverset of the matrix and then retrieves from
+##the cache when called next time for the same value
 
-## Write a short comment describing this function
+##This function creates a special object that stores a matrix in an environment different 
+##from the current environment
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        number <- NULL
+        get <- function() x
+        setinverse <- function(solve) number <<- solve
+        getinverse <- function() number
+        list(get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function cache's the inverse of the matrix and retrieves from the cache when called again
 
 cacheSolve <- function(x, ...) {
+        number <- x$getinverse()
+        if(!is.null(number))
+        {
+                message("Getting from cache")
+                return(number)
+        }
+        data <- x$get()
+        number <- solve(data,...)
+        x$setinverse(number)
+        number
+        
         ## Return a matrix that is the inverse of 'x'
 }
